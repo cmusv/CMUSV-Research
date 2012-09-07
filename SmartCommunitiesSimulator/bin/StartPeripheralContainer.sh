21ef65dee5ec1ddf4f2e7543275b1840482b1c36
mcsmith <michael.c.smith@alumni.cmu.edu>
2012-09-07 12:54:48 -0700

    Changed to use the new build directory structure.

--- SmartCommunitiesSimulator/bin/StartPeripheralContainer.sh
+++ SmartCommunitiesSimulator/bin/StartPeripheralContainer.sh
@@ -2,28 +2,24 @@
 #
 #   Starts the SmartCommunities agent(s).
 #
-SmartCommunitiesDatabaseHome=../SmartCommunitiesDatabase
-#HibernateHome=${SmartCommunitiesDatabaseHome}/external/hibernate-distribution-3.6.0.Final
-#MysqlHome=${SmartCommunitiesDatabaseHome}/external/mysql-connector-java-5.0.8
-Slf4jHome=external/slf4j-1.6.4
-Log4jHome=external/apache-log4j-1.2.16
-#
 #   Define the required class locations.
 #
-AgentClasspath=classes
-AgentClasspath=${AgentClasspath}:${SmartCommunitiesDatabaseHome}/classes
-#AgentClasspath=${AgentClasspath}:${HibernateHome}/hibernate3.jar
-#AgentClasspath=${AgentClasspath}:${HibernateHome}/lib/jpa/hibernate-jpa-2.0-api-1.0.0.Final.jar
-#AgentClasspath=${AgentClasspath}:${HibernateHome}/lib/required/antlr-2.7.6.jar
-#AgentClasspath=${AgentClasspath}:${HibernateHome}/lib/required/commons-collections-3.1.jar
-#AgentClasspath=${AgentClasspath}:${HibernateHome}/lib/required/dom4j-1.6.1.jar
-#AgentClasspath=${AgentClasspath}:${HibernateHome}/lib/required/javassist-3.12.0.GA.jar
-#AgentClasspath=${AgentClasspath}:${HibernateHome}/lib/required/jta-1.1.jar
-#AgentClasspath=${AgentClasspath}:${MysqlHome}/mysql-connector-java-5.0.8-bin.jar
-AgentClasspath=${AgentClasspath}:${Slf4jHome}/slf4j-api-1.6.4.jar
-#AgentClasspath=${AgentClasspath}:${Slf4jHome}/slf4j-jdk14-1.6.4.jar
-AgentClasspath=${AgentClasspath}:${Slf4jHome}/slf4j-log4j12-1.6.4.jar
-AgentClasspath=${AgentClasspath}:${Log4jHome}/log4j-1.2.16.jar
+AgentClasspath=build/classes
+AgentClasspath=${AgentClasspath}:build/lib/SmartCommunitiesDatabase.jar
+AgentClasspath=${AgentClasspath}:build/lib/antlr-2.7.7.jar
+AgentClasspath=${AgentClasspath}:build/lib/c3p0-0.9.1.jar
+AgentClasspath=${AgentClasspath}:build/lib/dom4j-1.6.1.jar
+AgentClasspath=${AgentClasspath}:build/lib/hibernate-c3p0-4.1.4.Final.jar
+AgentClasspath=${AgentClasspath}:build/lib/hibernate-commons-annotations-4.0.1.Final.jar
+AgentClasspath=${AgentClasspath}:build/lib/hibernate-core-4.1.4.Final.jar
+AgentClasspath=${AgentClasspath}:build/lib/hibernate-jpa-2.0-api-1.0.1.Final.jar
+AgentClasspath=${AgentClasspath}:build/lib/javassist-3.15.0-GA.jar
+AgentClasspath=${AgentClasspath}:build/lib/jboss-logging-3.1.0.GA.jar
+AgentClasspath=${AgentClasspath}:build/lib/jboss-transaction-api_1.1_spec-1.0.0.Final.jar
+AgentClasspath=${AgentClasspath}:build/lib/log4j-1.2.16.jar
+AgentClasspath=${AgentClasspath}:build/lib/mysql-connector-java-5.1.20-bin.jar
+AgentClasspath=${AgentClasspath}:build/lib/slf4j-api-1.6.4.jar
+AgentClasspath=${AgentClasspath}:build/lib/slf4j-log4j12-1.6.4.jar
 #
 #   Start the peripheral container using the provided configuration file.
 #
