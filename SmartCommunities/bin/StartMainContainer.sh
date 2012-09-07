258b00a30f0de31ac83e44743d3ac35f34c3aa69
mcsmith <michael.c.smith@alumni.cmu.edu>
2012-09-07 13:58:14 -0700

    Deleted files that have been migrated to other projects.

--- SmartCommunities/bin/StartMainContainer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-#   Starts a (backup or master) main container using the provided configuration.
-#
-java -cp ${JadeClasspath} -DJade.jar=${JadeJar} -DJadeSecurity.jar=${JadeSecurityJar} jade.Boot -conf "${1}"
