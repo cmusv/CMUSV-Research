0f25394e2e01a4e4cbc85e98b645d6fa4c24d747
mcsmith <michael.c.smith@alumni.cmu.edu>
2012-09-07 12:50:19 -0700

    Changed to take hostname from the environment variable, like other scripts.

--- SmartCommunitiesSimulator/scenarios/SecureAndRobustSenderAndReceiver/ConfigureEnvironment.sh
+++ SmartCommunitiesSimulator/scenarios/SecureAndRobustSenderAndReceiver/ConfigureEnvironment.sh
@@ -4,7 +4,7 @@
 #
 . scenarios/Common/ConfigureEnvironment.sh
 
-export BackupMainContainerHost=$(hostname)
+export BackupMainContainerHost=${HOSTNAME}
 export BackupMainContainerPort=1234
 export MasterMainContainerHost=community0node0
 export MasterMainContainerPort=1099
